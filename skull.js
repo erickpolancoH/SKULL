21af42c497d4a423611fb1e06b265d8b794199b4
SKULLPRIEST <77233656+erickpolancoH@users.noreply.github.com>
2023-06-15 18:28:35 -0400

    commit changes

--- skull.js
+++ skull.js
@@ -1,24 +1,17 @@
-const videoContainer = document.getElementById("videoContainer");
-const video = document.getElementById("video");
+const audio = document.getElementById("myAudio");
+const playBtn = document.getElementById("playBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+playBtn.addEventListener("click", () => {
+  audio.play();
+});
+pauseBtn.addEventListener("click", () => {
+  audio.pause();
+});
 
-function resizeVideo() {
-    const aspectRatio = video.videoWidth / video.videoHeight;
-    const newWidth = videoContainer.offsetWidth;
-    const newHeight = newWidth / aspectRatio;
-    video.style.width = newWidth + "px";
-    video.style.height = newHeight + "px";
-}
+// audioplayer purpose.
 
-window.addEventListener("resize", resizeVideo);
-resizeVideo();
 
-const videocontainer = document.getElementById("videocontainer");
 
-// enable inline playback
-video.setAttribute("playsinline", "");
 
-// enable autoplay
-video.setAttribute("autoplay", "");
 
-// start playing the video
-video.play();
+
