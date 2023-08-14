65314ed21632529ea71e266dec2d3f9c263d8c46
erickpolancoH <bizarrecartoon@gmail.com>
2023-08-13 19:02:23 -0700

    commit

--- skull.js
+++ skull.js
@@ -12,27 +12,4 @@ pauseBtn.addEventListener("click", () => {
 
 
 
-// buttons propperties
 
-
-
-        // Obtén los botones por su ID
-        const button1 = document.getElementById('button1');
-        const button2 = document.getElementById('button2');
-        const button3 = document.getElementById('button3');
-
-        // Agrega un evento de clic a cada botón
-        button1.addEventListener('click', function() {
-            // Redirige a la página skull1.html
-            window.location.href = 'skull.html';
-        });
-
-        button2.addEventListener('click', function() {
-            // Redirige a la página skull2.html
-            window.location.href = 'skull2.html';
-        });
-
-        button3.addEventListener('click', function() {
-            // Redirige a la página skull3.html
-            window.location.href = 'skull3.html';
-        });
